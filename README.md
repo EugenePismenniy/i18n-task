9b27996b9098eccff52384add0ec3394118f9604
eugene <evgeniy.pismenny@gmail.com>
2017-01-13 17:08:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+###Internationalization welcome messages
+
+The simple console application to demonstrate the working with internationalization 
---
+++ src/main/java/ua/home/i18n/goodmsg/MainClass.java
@@ -0,0 +1,11 @@
+package ua.home.i18n.goodmsg;
+
+/**
+ * @author it100985pev on 13.01.17 17:00.
+ */
+public class MainClass {
+
+	public static void main(String[] args) {
+
+	}
+}
